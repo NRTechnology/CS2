61bc915b2a0e4e7650474b2eeca33687ec30b59d
NR Technology <nrtechnologyid@gmail.com>
2020-03-25 10:34:31 +0700

    Pertemuan 4

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
+# ini adalah pertemuan 3
 @app.route('/')
 def hello_world():
     return 'Selamat datang'
@@ -12,6 +15,19 @@ def hello_world():
 def admin_page():
     return 'Ini adalah halaman admin'
 
+
+# materi pertemuan 3 berakhir di sini
+
+
+# ini adalah materi pertemuan 4
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+api.add_resource(HelloWorld, '/helloworld')
+
+# materi pertemuan 4 berakhir di sini
 
 if __name__ == '__main__':
     app.run()
