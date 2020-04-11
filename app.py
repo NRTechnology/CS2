01a2f4ba46471fbb86130f7b4dc3028fe85c705f
NR Technology <nrtechnologyid@gmail.com>
2020-04-11 15:44:20 +0700

    Pertemuan 7

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, Api
-from flask import Flask, Response, json, jsonify
+from flask import Flask, Response, json, jsonify, request, abort
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -30,6 +30,7 @@ def admin_page():
 # ini adalah materi pertemuan 4
 
 class HelloWorld(Resource):
+    @staticmethod
     def get(self):
         return {'hello': 'world'}
 
@@ -39,7 +40,7 @@ api.add_resource(HelloWorld, '/helloworld')
 
 # materi pertemuan 4 berakhir di sini
 
-# ini adalamat materi pertemuan 5
+# ini adalah materi pertemuan 5
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
@@ -57,14 +58,14 @@ class User(db.Model):
 class UserSchema(ma.Schema):
     class Meta:
         # Fields to expose
-        fields = ('username', 'email')
+        fields = ('id', 'username', 'email')
 
 
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
 
-@app.route("/user", methods=["GET"])
+@app.route("/user/", methods=["GET"])
 def get_user():
     all_users = User.get_all_users()
     result = users_schema.dump(all_users)
@@ -72,6 +73,49 @@ def get_user():
 
 
 # materi pertemuan 5 berakhir di sini
+
+
+@app.route("/user/<int:id>/", methods=["GET"])
+def get_one_user(id):
+    user = User.query.get(id)
+    result = user_schema.dump(user)
+    return jsonify(result)
+
+
+@app.route("/user/", methods=["POST"])
+def create_user():
+    if not request.json or not 'username' in request.json and not 'email' in request.json:
+        abort(400)
+
+    user = User(request.json['username'], request.json['email'])
+    db.session.add(user)
+    db.session.commit()
+
+    result = user_schema.dump(user)
+    return jsonify(result)
+
+
+@app.route("/user/<int:id>/", methods=["PUT"])
+def update_user(id):
+    if not request.json or not 'username' in request.json and not 'email' in request.json:
+        abort(400)
+
+    user = User.query.get(id)
+    user.username = request.json['username']
+    user.email = request.json['email']
+    db.session.commit()
+
+    result = user_schema.dump(user)
+    return jsonify(result)
+
+
+@app.route("/user/<int:id>/", methods=["DELETE"])
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify()
 
 
 if __name__ == '__main__':
